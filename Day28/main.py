8542580955246a9e49216e2a4cb60debc9adc997
sheylacantalupo <sheylacantalupo.sc@gmail.com>
2022-09-23 14:43:17 -0300

    pomodoro

--- Day28/main.py
+++ Day28/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageTk
@@ -11,14 +12,57 @@ fonte = "Courier"
 trabalho = 25
 pausa_curta = 5
 pausa_longa = 20
-
+reps = 0
+Work_min = 25
+Short_break_min = 5
+Long_break_min = 20
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    label.config(text="TIMER")
+    check.config(text="")
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = Work_min * 60
+    short_break_sec = Short_break_min * 60
+    long_break_sec = Long_break_min * 60
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label.config(text="BREAK", foreground=vermelho)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="BREAK", foreground=rosa)
+    else:
+        count_down(work_sec)
+        label.config(text="WORK", foreground=verde)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    min = math.floor(count/60)
+    sec = count % 60
+    if sec <10:
+        sec = f"0{sec}"
 
-#
+    canvas.itemconfig(timer_text, text=f"{min}:{sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+            check.config(text=mark)
+
+
 #---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("POMODORO")
@@ -31,18 +75,18 @@ label.grid(column=1, row=0)
 canvas = Canvas(width=220, height=224, bg=amarelo, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(110, 130, text="00:00", fill="white", font=(fonte, 35, "bold"))
+timer_text = canvas.create_text(110, 130, text="00:00", fill="white", font=(fonte, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check = Label(text="✔", background=amarelo, foreground=verde)
+check = Label(background=amarelo, foreground=verde)
 check.grid(column=1, row=3)
 
 
-
+text="✔",
 window.mainloop()
